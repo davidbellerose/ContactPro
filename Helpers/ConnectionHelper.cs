4ccf6843b0cbc14434c5a7acaae5eba76902a753
daveyedgar <62836024+daveyedgar@users.noreply.github.com>
2023-05-13 16:50:18 -0400

    updated connection string

--- Helpers/ConnectionHelper.cs
+++ Helpers/ConnectionHelper.cs
@@ -15,39 +15,39 @@ namespace ContactPro.Helpers
         }
 
         // build connection string from environment for Raleway
-        //private static string BuildConnectionString(string databaseUrl)
-        //{
-        //    var databaseUri = new Uri(databaseUrl);
-        //    var userInfo = databaseUri.UserInfo.Split(':');
-        //    var builder = new NpgsqlConnectionStringBuilder
-        //    {
-        //        Host = databaseUri.Host,
-        //        Port = databaseUri.Port,
-        //        Username = userInfo[0],
-        //        Password = userInfo[1],
-        //        Database = databaseUri.LocalPath.TrimStart('/'),
-        //        SslMode = SslMode.Require,
-        //        TrustServerCertificate = true
-        //    };
-
-        //    return builder.ToString();
-        //}
-
-        private static string BuildConnectionString(string databaseurl)
+        private static string BuildConnectionString(string databaseUrl)
         {
-            var databaseuri = new Uri(databaseurl);
-            var userInfo = databaseuri.UserInfo.Split(':');
-
-            return new NpgsqlConnectionStringBuilder()
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+            var builder = new NpgsqlConnectionStringBuilder
             {
-                Host = databaseuri.Host,
-                Port = databaseuri.Port,
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
                 Username = userInfo[0],
                 Password = userInfo[1],
-                Database = databaseuri.LocalPath.TrimStart('/'),
-                SslMode = SslMode.Prefer,
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
                 TrustServerCertificate = true
-            }.ToString();
+            };
+
+            return builder.ToString();
         }
+
+        //private static string BuildConnectionString(string databaseurl)
+        //{
+        //    var databaseuri = new Uri(databaseurl);
+        //    var userInfo = databaseuri.UserInfo.Split(':');
+
+        //    return new NpgsqlConnectionStringBuilder()
+        //    {
+        //        Host = databaseuri.Host,
+        //        Port = databaseuri.Port,
+        //        Username = userInfo[0],
+        //        Password = userInfo[1],
+        //        Database = databaseuri.LocalPath.TrimStart('/'),
+        //        SslMode = SslMode.Prefer,
+        //        TrustServerCertificate = true
+        //    }.ToString();
+        //}
     }
 }
